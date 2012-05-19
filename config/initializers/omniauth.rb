3695ade31157c637c71aa16844208c7e25fa563b
Winfred <winfred.nadeau@gmail.com>
2012-05-19 13:10:16 -0400

    conditionally use ENV vars for mailchimp oauth key/secret (for production)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :mailchimp, '384373877895', '51918a815c64ee2bf209c2fb717262b8'
+  provider :mailchimp, 
+  (ENV['MAILCHIMP_CLIENT_KEY'] || '384373877895'), 
+  (ENV['MAILCHIMP_CLIENT_SECRET'] ||'51918a815c64ee2bf209c2fb717262b8')
 end
